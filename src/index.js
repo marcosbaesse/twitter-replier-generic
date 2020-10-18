ebdb0f3eb10f763982208b5d8c16fb47921e3c99
Marcos Baesse <marcosbaesse@gmail.com>
2020-10-18 16:40:11 -0300

    Era pra funcionar mas não tá , sei la, acho que chega por hoje

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 require('dotenv').config();
 const Twitter = require('twitter');
 
-var client = new Twitter({
+const client = new Twitter({
     consumer_key: process.env.TWITTER_API_KEY,
     consumer_secret: process.env.TWITTER_API_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-client.post('statuses/update', { status: 'Roi... Twitter, né?' }, function(error, tweet, response) {
-    if (error) {
-        console.error(error);
-    }
+client.stream("statuses/filter", { track: "@FlerteB" }, function(stream) {
+    stream.on("data", function(tweet) {
+        const reply = {
+            status: "Você não é o steven, mas é meu universo :D",
+            in_reply_to_status_id: tweet.id_str
+        };
 
-    console.log(tweet);
-  });
+        client.post("statuses/update", reply, function(error, tweetReply, response) {
+            if (error) {
+                console.log({ error, reply, response });
+            }
 
+            console.log({ tweetReply });
+        });
+    });
+
+    stream.on("error", function(error) {
+        console.log({ "streamError": error });
+    });
+});
