68fb7f40ca0e363b9fa9fb4363807996dfba2db3
Marcos Baesse <marcosbaesse@gmail.com>
2020-10-18 15:58:20 -0300

    Add request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-const axios = require('axios').default;
+require('dotenv').config();
+const Twitter = require('twitter');
 
-axios.get("https://api.twitter.com/2/tweets/20", {
-    headers: { "Authorization": "Bearer <>" }
-})
-.then(console.log);
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+client.post('statuses/update', { status: 'Roi... Twitter, né?' }, function(error, tweet, response) {
+    if (error) {
+        console.error(error);
+    }
+
+    console.log(tweet);
+  });
+
