23620d029cac03cfa9b193b8ffe13a1ac7ad637a
David Campos <fernandesc.david@gmail.com>
2021-10-26 12:19:46 -0300

    refactor: add class TwitterClient (#10)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { config } from 'dotenv';
 config();
-import Twitter from 'twitter';
 
-const lerolero = require('lerolero');
+import Twitter from 'twitter';
+import TwitterClient from './twitter-client';
 
 const credentials = {
     consumer_key: process.env.TWITTER_API_KEY,
@@ -11,35 +11,5 @@ const credentials = {
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 } as Twitter.AccessTokenOptions;
 
-const client = new Twitter(credentials);
-
-client.stream("statuses/filter", { track: "@MarcosBBot" }, (stream): void => {
-    console.log("listening...");
-
-    stream.on("data", (tweet): void => {
-        let replyMessage = lerolero().toLowerCase();
-
-        const reply = {
-            status: `Oi @${tweet.user.screen_name}, ${replyMessage}`,
-            in_reply_to_status_id: tweet.id_str
-        };
-
-        if(reply.status.length>250){
-            reply.status = reply.status.slice(0,250-reply.status.length)
-        }
-
-        console.log({ reply });
-
-        client.post("statuses/update", reply, (error, tweetReply, response): void => {
-            if (error) {
-                console.log({ error, reply, response });
-            }
-
-            console.log({ tweetReply });
-        });
-    });
-
-    stream.on("error", (error): void => {
-        console.log({ "streamError": error });
-    });
-});
+const client = new TwitterClient(credentials);
+client.stream();
