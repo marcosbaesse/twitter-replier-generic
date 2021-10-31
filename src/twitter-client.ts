a5b1b42cb87c5574bd0a7785b9e63c15a7ead192
David Campos <fernandesc.david@gmail.com>
2021-10-31 15:37:03 -0300

    refactor class TwitterClient

--- src/twitter-client.ts
+++ src/twitter-client.ts
@@ -1,5 +1,11 @@
 import Twitter, { AccessTokenOptions } from 'twitter';
 
+interface Tweet {
+    id_str: string;
+    user: {
+        screen_name: string;
+    }
+}
 class TwitterClient {
 
     private client: Twitter;
@@ -8,24 +14,30 @@ class TwitterClient {
         this.client = new Twitter(credentials);
     }
 
+    replyGenerate(tweet: Tweet): object {
+        const lerolero = require('lerolero');
+        let replyMessage = lerolero().toLowerCase();
+
+        const reply = {
+            status: `Oi @${tweet.user.screen_name}, ${replyMessage}`,
+            in_reply_to_status_id: tweet.id_str
+        };
+
+        if (reply.status.length > 250) {
+            reply.status = reply.status.slice(0, 250 - reply.status.length)
+        }
+
+        console.log({ reply });
+
+        return reply;
+    }
+
     stream(): void {
         this.client.stream("statuses/filter", { track: "@MarcosBBot" }, (stream: any): void => {
             console.log("listening...");
 
             stream.on("data", (tweet: any): void => {
-                const lerolero = require('lerolero');
-                let replyMessage = lerolero().toLowerCase();
-
-                const reply = {
-                    status: `Oi @${tweet.user.screen_name}, ${replyMessage}`,
-                    in_reply_to_status_id: tweet.id_str
-                };
-
-                if (reply.status.length > 250) {
-                    reply.status = reply.status.slice(0, 250 - reply.status.length)
-                }
-
-                console.log({ reply });
+                const reply = this.replyGenerate(tweet);
 
                 this.client.post("statuses/update", reply, (error: any, tweetReply: any, response: any): void => {
                     if (error) {
