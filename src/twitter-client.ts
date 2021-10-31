113d0cb2e1c9951bf5b378866d9aaf1767f258b6
David Campos <fernandesc.david@gmail.com>
2021-10-31 17:00:47 -0300

    add dynamic track (#12)
    * refactor: add class TwitterClient
    
    * refactor class TwitterClient
    
    * add dynamic track
    
    * Revert "add dynamic track"
    
    This reverts commit 0e99624f3aefcfc5b863cb8dbcdb306e3792fcb3.
    
    * add dynamic track

--- src/twitter-client.ts
+++ src/twitter-client.ts
@@ -1,31 +1,45 @@
 import Twitter, { AccessTokenOptions } from 'twitter';
 
+interface Tweet {
+    id_str: string;
+    user: {
+        screen_name: string;
+    }
+}
 class TwitterClient {
 
     private client: Twitter;
+    private track: string;
 
     constructor(credentials: AccessTokenOptions) {
         this.client = new Twitter(credentials);
+        this.track = process.env.TWITTER_TRACK || "@MarcosBBot";
+    }
+
+    replyGenerate(tweet: Tweet): object {
+        const lerolero = require('lerolero');
+        let replyMessage = lerolero().toLowerCase();
+
+        const reply = {
+            status: `Oi @${tweet.user.screen_name}, ${replyMessage}`,
+            in_reply_to_status_id: tweet.id_str
+        };
+
+        if (reply.status.length > 250) {
+            reply.status = reply.status.slice(0, 250 - reply.status.length)
+        }
+
+        console.log({ reply });
+
+        return reply;
     }
 
     stream(): void {
-        this.client.stream("statuses/filter", { track: "@MarcosBBot" }, (stream: any): void => {
+        this.client.stream("statuses/filter", { track: this.track }, (stream: any): void => {
             console.log("listening...");
 
             stream.on("data", (tweet: any): void => {
-                const lerolero = require('lerolero');
-                let replyMessage = lerolero().toLowerCase();
-
-                const reply = {
-                    status: `Oi @${tweet.user.screen_name}, ${replyMessage}`,
-                    in_reply_to_status_id: tweet.id_str
-                };
-
-                if (reply.status.length > 250) {
-                    reply.status = reply.status.slice(0, 250 - reply.status.length)
-                }
-
-                console.log({ reply });
+                const reply = this.replyGenerate(tweet);
 
                 this.client.post("statuses/update", reply, (error: any, tweetReply: any, response: any): void => {
                     if (error) {
